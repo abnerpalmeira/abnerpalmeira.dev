21bdefead51b1b62b25a3c80cf5d259085346828
Abner <abnerpalmeira@gmail.com>
2020-06-14 17:04:46 -0300

    style: removes app stylesheet

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,4 @@ body{
   overflow: hidden;
   height: 100vh;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
