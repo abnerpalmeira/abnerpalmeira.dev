e21144d1c1a726a0d69fb1cd6eb38994fbdc8899
Abner <abnerpalmeira@gmail.com>
2020-06-14 17:07:48 -0300

    feat: creates a function to animate the polygons

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -1,20 +1,27 @@
 <template>
-  <svg height="100vh" width="auto" viewBox="0 0 385 531" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
-    <polygon v-for="polygon in polygons" :key="polygon"
+  <svg v-on:click="animate" height="100vh" width="100%" viewBox="0 0 385 531" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+    <polygon 
+      v-for="(polygon,index) in randomizedPolygons" :key="`polygon-${index}`"
       :points="convertPoints(polygon.data)"
-      :fill="convertColor(polygon.color)" 
+      :fill="convertColor(polygon.color)"
+      opacity="0.51"
+      :id="`polygon-${index}`" 
     />
   </svg>
 </template>
 
 <script>
+
+import {TimelineMax,Elastic} from 'gsap'
 import json from './polygons.json'
+
 export default {
   name: 'Animation',
   data(){
     return{
       polygons: json,
       randomizedPolygons: [],
+      timeLine: new TimelineMax(),
       windowWidth: window.screen.width,
       windowHeight: window.screen.height
     }
@@ -23,10 +30,12 @@ export default {
 
     this.randomizedPolygons = JSON.parse(JSON.stringify(this.polygons))
     for (var i = 0; i < this.randomizedPolygons.length; i++) {
+
       let data = this.randomizedPolygons[i]['data'].slice();
 
-      let newX = this.randomInt(0,this.windowWidth)
-      let newY = this.randomInt(0,this.windowHeight)
+      let newX = this.randomInt(-this.windowWidth*0.50,this.windowWidth)
+      let newY = this.randomInt(-this.windowHeight*0.50,this.windowHeight)
+
       this.randomizedPolygons[i]['data'][0] = newX
       this.randomizedPolygons[i]['data'][1] = newY
 
@@ -41,15 +50,29 @@ export default {
     }
   },
   methods: {
+    animate: function() {
+      for (var i = 0; i < 100; i++) {
+        this.timeLine.to(
+          `#polygon-${i}`,
+          0.1,
+          {
+            attr: {
+              points: this.convertPoints(this.polygons[i]['data'])
+            },
+            ease:Elastic.easeOut
+          })
+      }
+      //alert('cliquei')
+    },
+    convertPoints: function(data) {
+      return `${data[0]},${data[1]} ${data[2]},${data[3]} ${data[4]},${data[5]}`
+    },
+    convertColor: function(color) {
+      return `rgb(${color[0]},${color[1]},${color[2]})`
+    },
     randomInt: function(min, max) {
       return min + Math.floor((max - min) * Math.random());
     },
-    convertPoints: function(data) {
-      return data[0]+','+data[1]+' '+data[2]+','+data[3]+' '+data[4]+','+data[5]
-    },
-    convertColor: function(color) {
-      return 'rgb('+color[0]+','+color[1]+','+color[2]+')'
-    }
   },
   props: {
     msg: String
